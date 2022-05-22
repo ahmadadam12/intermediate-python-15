794c4d0630a49d351e7849e626ad2591b58fc5c4
Ahmadadam12 <adam.zam>
2022-05-22 23:25:28 +0700

    trial

--- numpy_trial.py
+++ numpy_trial.py
@@ -36,6 +36,30 @@ print(arr2,arr2.shape)
 arr3 = np.arange(1,21).reshape(5,2,2)
 #print(arr3)
 arr4 = np.arange(21,41).reshape(5,2,2)
-arr5 = np.concatenate((arr3,arr4),axis = 2)
-print(arr5,arr5.shape)
-print("\nslicing: \n",arr5[1:3,:,1:])
+#arr5 = np.concatenate((arr3,arr4),axis = 2)
+#print(arr5,arr5.shape)
+#print("\nslicing: \n",arr5[1:3,:,1:])
+#print("\nslicing: \n",arr5[1,:,1:] + arr5[4,:,1:])
+
+"""
+arr6 = np.concatenate((arr3,arr4),axis = 1)
+print(arr6,"shape:",arr6.shape)
+print(arr6[2,:,:],"\n",arr6[3,:,:])
+arr7 = np.concatenate((arr6[2,:,:],arr6[3,:,:]),axis = 1)
+print(arr7) 
+"""
+
+"""
+arr1 = np.array([[1,2],
+                [1,2]])
+arr2 = np.array([[3,4],
+                [3,4]])
+arr3 = arr1@arr2
+print(arr3,arr3.mean())
+"""
+
+
+arr1 = np.array([[1,2,3,4],
+                [5,6,7,8],
+                [9,10,11,12]],dtype = np.int32)
+print(arr1[::2,1::2])
